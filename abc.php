74061e5417bca84f09ad30296518107c1622fc2b
AlminaRascic <62564746+AlminaRascic@users.noreply.github.com>
2021-01-05 11:43:10 +0100

    Create abc.php

---
+++ abc.php
@@ -0,0 +1,30 @@
+$conn = mysqli_connect('localhost:3308', 'root', '');
+if (!$conn){
+    die("Database Connection Failed" . mysqli_error($conn));
+}
+$select_db = mysqli_select_db($conn, 'resume');
+if (!$select_db){
+    die("Database Selection Failed" . mysqli_error($conn));
+}   
+
+if((isset($_POST['name'])) && (isset($_POST['email'])))
+<?php 
+{
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$comment = $_POST['comment'];
+
+
+$sql="INSERT INTO feedback (name, email, comment) VALUES ('$name','$email', '$comment');";
+$result = mysqli_query($conn, $sql);
+
+if($result){
+    
+echo "Thank you! We will get in touch with you soon";
+}
+else{
+    echo "Submission failed!";
+}    
+}
+?>
